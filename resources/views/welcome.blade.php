360d545c0bea4f53b66b3bf0a0f52d3d923b6adb
renzocarara <renzo.carara@libero.it>
2021-01-19 17:59:06 +0100

    adjusted style on welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,10 +31,10 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('login') }}" class="text-lg text-gray-700 underline">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-lg text-gray-700 underline">Register</a>
                         @endif
                     @endauth
                 </div>
